6ef83b101a557b3bb567812745a8ecba88b64e11
Karey Higuera <karey.higuera@gmail.com>
2020-12-19 11:59:15 -0600

    add moviemon python package

--- programs/python3.sh
+++ programs/python3.sh
@@ -5,3 +5,7 @@ source ../util.sh
 echo "🐍 Installing Python"
 install python3-venv
 install python3-pip
+
+echo "Installing pip packages"
+pip3 install moviemon
+pip3 install stevedore==1.19.1 # Dependency update for moviemon
