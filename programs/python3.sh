6dfb7e69aff6b2369d57c644152291b7673fd21f
Karey Higuera <karey.higuera@gmail.com>
2020-12-28 09:05:28 -0600

    add beets music tool

--- programs/python3.sh
+++ programs/python3.sh
@@ -7,5 +7,6 @@ install python3-venv
 install python3-pip
 
 echo "Installing pip packages"
+pip3 install https://github.com/beetbox/beets/tarball/master
 pip3 install moviemon
 pip3 install stevedore==1.19.1 # Dependency update for moviemon
