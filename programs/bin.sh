525b03779ca20a4fa6f4c9a49415e743c673295a
Karey Higuera <karey.higuera@gmail.com>
2023-02-22 09:38:50 -0500

    clean up dangerous dotfile

--- programs/bin.sh
+++ programs/bin.sh
@@ -1,7 +1,9 @@
 # Symlink bin scripts to local bin folder
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-for program in ../bin/*; do
-  sudo rm -f "/usr/bin/${program}.bak"
-  sudo mv "/usr/bin/${program}" "/usr/bin/${program}.bak"
-  sudo ln -s "../bin/${program}" "/usr/bin/${program}"
+for program_path in $SCRIPT_DIR/../bin/*; do
+  program=$(basename $program_path)
+  rm -f "$HOME/.local/bin/${program}.bak"
+  [ -e "$HOME/.local/bin/${program}" ] && mv "$HOME/.local/bin/${program}" "$HOME/.local/bin/${program}.bak"
+  ln -s "$program_path" "$HOME/.local/bin/${program}"
 done
