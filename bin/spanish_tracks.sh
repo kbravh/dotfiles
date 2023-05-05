554dd5c70aefd7de4d7c74e13311ccf2bfcadcd7
Karey Higuera <karey.higuera@gmail.com>
2023-05-04 20:46:08 -0400

    add script for finding videos with spanish audio tracks

---
+++ bin/spanish_tracks.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Directory containing the video files
+dir="$1"
+
+# Loop over all .mp4 and .mkv files in the directory
+for file in "$dir"/*.{mp4,mkv}; do
+  # Check if the file exists (this handles the case where there are no files with the given extension)
+  if [ -f "$file" ]; then
+    # Use ffprobe to list the metadata for each audio stream in the file
+    # and grep to check for the presence of a Spanish audio track
+    if ffprobe -v quiet -select_streams a -show_streams "$file" | grep -q "TAG:language=spa"; then
+      echo "Spanish audio track found in: $(basename "$file")"
+    fi
+  fi
+done
+
