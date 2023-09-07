b4e81bf93389b46b31f200cd14d5a8d7e7207e5d
Karey Higuera <karey.higuera@churchofjesuschrist.org>
2023-09-07 09:18:22 -0400

    add espanso install

--- install.sh
+++ install.sh
@@ -2,12 +2,18 @@
 
 echo "Installing applications with brew"
 
+# Taps
+brew tap espanso/espanso
+
+# Apps
 brew install bat
 brew install bat-extras
 brew install ripgrep
 brew install fd
 brew install jq
+brew install espanso
 
 # Casks
 brew install --cask flameshot
 brew install --cask amethyst
+
