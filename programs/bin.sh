17e3d4c69a58d0f5a865581181cc5e18f6ef42d4
Karey Higuera <karey.higuera@gmail.com>
2024-06-23 20:39:24 -0400

    update bin install

--- programs/bin.sh
+++ programs/bin.sh
@@ -1,6 +1,9 @@
 # Symlink bin scripts to local bin folder
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
+# make local bin folder if it does not exist
+mkdir -p "$HOME/.local/bin"
+
 for program_path in $SCRIPT_DIR/../bin/*; do
   program=$(basename $program_path)
   rm -f "$HOME/.local/bin/${program}.bak"
