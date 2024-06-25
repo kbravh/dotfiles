b2aa877beb7cb1a045f88478b4dedbb89e42d9d0
Karey Higuera <karey.higuera@gmail.com>
2024-06-25 09:55:03 -0400

    remove neovim install

--- programs/neovim.sh
+++
@@ -1,5 +0,0 @@
-#!bin/bash
-
-wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.deb #download
-sudo apt install ./nvim-linux64.deb # install
-rm ./nvim-linux64.deb # cleanup
