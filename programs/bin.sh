0c858009c2daa7d7f172c842b97d8108502fae80
Karey Higuera <karey.higuera@gmail.com>
2024-06-25 09:34:57 -0400

    make local bin folder

--- programs/bin.sh
+++ programs/bin.sh
@@ -1,6 +1,9 @@
 # Symlink bin scripts to local bin folder
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
+# make bin dir
+mkdir -p "$HOME/.local/bin"
+
 for program_path in $SCRIPT_DIR/../bin/*; do
   program=$(basename $program_path)
   rm -f "$HOME/.local/bin/${program}.bak"
